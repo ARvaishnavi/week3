3426ea6475155f9e689a70e6cc949a0e5308eb85
ARvaishnavi <141898980+ARvaishnavi@users.noreply.github.com>
2023-08-11 10:06:55 +0530

    examples.java
    exaples added

---
+++ examples.java
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        header{
+            position: fixed;
+            top:0px;
+            left:0px;
+            background-color: rgb(169, 240, 57);
